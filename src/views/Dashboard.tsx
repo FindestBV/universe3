5a2322e175a0f3cc0fb919f180d4f3f33c7cb4cb
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2024-12-13 15:47:53 +0100

    third force merge

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -5,14 +5,14 @@ import {
     Avatar,
     AvatarFallback,
     AvatarImage,
-  } from "@/components/ui/avatar"
+} from "@/components/ui/avatar"
 import { useNavigate } from 'react-router-dom';
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu"
 import DataChart from "@/components/data-chart";
 import { useTranslation } from 'react-i18next';
 // import { useSelector } from 'react-redux';
@@ -25,13 +25,13 @@ export const Dashboard = () => {
     const navigate = useNavigate();
     // const user = useSelector(currentUser);
 
-    const handleNavigateToEntities = (type:string, id:string) => {
-        let redirRoute; 
-        if(type == 'Entity'){
-            redirRoute = 'entities'; 
+    const handleNavigateToEntities = (type: string, id: string) => {
+        let redirRoute;
+        if (type == 'Entity') {
+            redirRoute = 'entities';
         } else {
-            redirRoute = 'studies'; 
-        }; 
+            redirRoute = 'studies';
+        };
         navigate(`/library/${redirRoute}/${id}`, { state: { id } });
     };
 
@@ -48,103 +48,103 @@ export const Dashboard = () => {
         return `${hours}:${minutes}`;
     };
 
-    return(
+    return (
         <div className="flex flex-col w-full h-screen max-sm:px-4" style={{ padding: "16px 45px 0" }}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
 
-            <div  className="flex flex-col space-y-3 w-full max-sm:px-4 overflow-hidden">
-                <h2 className='overViewTitle'>{t('pickup')}</h2>
-                <div className="flex flex-col items-start justify-start h-[350px] w-full gap-2 overflow-y-scroll">
-                    {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
-                    {activityData && activityData.map((activity:any, idx:string) => (
-                        <div 
-                            key={idx} 
-                            className="flex flex-row items-start w-full max-sm:px-4 bg-[#e5f7fe] rounded-sm px-4 py-2 cursor-pointer shadow-md" 
-                            onClick={() => handleNavigateToEntities(activity.type, activity.id)}
-                        >
-                            <div className='flex flex-col justify-between w-full items-start'>
-                                <div className="flex items-center justify-between w-full">
-                                    <p className="flex gap-2 overViewText">
-                                        <svg 
-                                            aria-hidden="true" 
-                                            focusable="false" 
-                                            data-prefix="fas" 
-                                            data-icon="dice-d6" 
-                                            className="svg-inline--fa fa-dice-d6 objectItem_objectIcon__xwkQs" 
-                                            width="12px" 
-                                            role="img" 
-                                            xmlns="http://www.w3.org/2000/svg" 
-                                            viewBox="0 0 448 512"
-                                        >
-                                            <path 
-                                                fill="currentColor" 
-                                                d="M201 10.3c14.3-7.8 31.6-7.8 46 0L422.3 106c5.1 2.8 8.3 8.2 8.3 14s-3.2 11.2-8.3 14L231.7 238c-4.8 2.6-10.5 2.6-15.3 0L25.7 134c-5.1-2.8-8.3-8.2-8.3-14s3.2-11.2 8.3-14L201 10.3zM23.7 170l176 96c5.1 2.8 8.3 8.2 8.3 14l0 216c0 5.6-3 10.9-7.8 13.8s-10.9 3-15.8 .3L25 423.1C9.6 414.7 0 398.6 0 381L0 184c0-5.6 3-10.9 7.8-13.8s10.9-3 15.8-.3zm400.7 0c5-2.7 11-2.6 15.8 .3s7.8 8.1 7.8 13.8l0 197c0 17.6-9.6 33.7-25 42.1L263.7 510c-5 2.7-11 2.6-15.8-.3s-7.8-8.1-7.8-13.8l0-216c0-5.9 3.2-11.2 8.3-14l176-96z"
-                                            ></path>
-                                        </svg>
-                                        {activity.type}
-                                    </p>
+                <div className="flex flex-col space-y-3 w-full max-sm:px-4 overflow-hidden">
+                    <h2 className='overViewTitle'>{t('pickup')}</h2>
+                    <div className="flex flex-col items-start justify-start h-[350px] w-full gap-2 overflow-y-scroll">
+                        {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+                        {activityData && activityData.map((activity: any, idx: string) => (
+                            <div
+                                key={idx}
+                                className="flex flex-row items-start w-full max-sm:px-4 bg-[#e5f7fe] rounded-sm px-4 py-2 cursor-pointer shadow-md"
+                                onClick={() => handleNavigateToEntities(activity.type, activity.id)}
+                            >
+                                <div className='flex flex-col justify-between w-full items-start'>
+                                    <div className="flex items-center justify-between w-full">
+                                        <p className="flex gap-2 overViewText">
+                                            <svg
+                                                aria-hidden="true"
+                                                focusable="false"
+                                                data-prefix="fas"
+                                                data-icon="dice-d6"
+                                                className="svg-inline--fa fa-dice-d6 objectItem_objectIcon__xwkQs"
+                                                width="12px"
+                                                role="img"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 448 512"
+                                            >
+                                                <path
+                                                    fill="currentColor"
+                                                    d="M201 10.3c14.3-7.8 31.6-7.8 46 0L422.3 106c5.1 2.8 8.3 8.2 8.3 14s-3.2 11.2-8.3 14L231.7 238c-4.8 2.6-10.5 2.6-15.3 0L25.7 134c-5.1-2.8-8.3-8.2-8.3-14s3.2-11.2 8.3-14L201 10.3zM23.7 170l176 96c5.1 2.8 8.3 8.2 8.3 14l0 216c0 5.6-3 10.9-7.8 13.8s-10.9 3-15.8 .3L25 423.1C9.6 414.7 0 398.6 0 381L0 184c0-5.6 3-10.9 7.8-13.8s10.9-3 15.8-.3zm400.7 0c5-2.7 11-2.6 15.8 .3s7.8 8.1 7.8 13.8l0 197c0 17.6-9.6 33.7-25 42.1L263.7 510c-5 2.7-11 2.6-15.8-.3s-7.8-8.1-7.8-13.8l0-216c0-5.9 3.2-11.2 8.3-14l176-96z"
+                                                ></path>
+                                            </svg>
+                                            {activity.type}
+                                        </p>
+                                    </div>
+                                    <h3 className="font-bold">{activity.name}</h3>
                                 </div>
-                                <h3 className="font-bold">{activity.name}</h3>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger asChild>
+                                        <Button variant="rotated" className="h-8 w-8 p-0 bg-transparent">
+                                            <span className="sr-only">Open menu</span>
+                                            <MoreHorizontal />
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent align="end">
+                                        <DropdownMenuItem>
+                                            <SquareArrowOutUpRight /> Open Page
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <ScanEye /> Open Preview
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <Network /> Open in Tree View
+                                        </DropdownMenuItem>
+                                        <DropdownMenuItem>
+                                            <List /> Open in List View
+                                        </DropdownMenuItem>
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
                             </div>
-                            <DropdownMenu>
-                                <DropdownMenuTrigger asChild>
-                                    <Button variant="rotated" className="h-8 w-8 p-0 bg-transparent">
-                                        <span className="sr-only">Open menu</span>
-                                        <MoreHorizontal />
-                                    </Button>
-                                </DropdownMenuTrigger>
-                                <DropdownMenuContent align="end">
-                                    <DropdownMenuItem>
-                                        <SquareArrowOutUpRight /> Open Page
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem>
-                                        <ScanEye /> Open Preview
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem>
-                                        <Network /> Open in Tree View
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem>
-                                        <List /> Open in List View
-                                    </DropdownMenuItem>
-                                </DropdownMenuContent>
-                            </DropdownMenu>
-                        </div>
-                    ))}
-                </div>
-            </div>
-    
-            <div  className="flex flex-col space-y-3 w-full max-sm:px-4">
-                <h2 className='overViewTitle'>{t('relationsGraph')}</h2>
-                <div className="flex items-center justify-center  animate-pulse h-auto w-full rounded-xl">
-                  <DataChart />
-                </div>
-            </div>
-    
-            <div  className="flex flex-col space-y-3 w-full max-sm:px-4 overflow-hidden">
-                <h2 className='overViewTitle'>{t('happening')}</h2>
-                <div className="flex flex-col items-start justify-start h-[300px] w-full rounded-xl gap-1 overflow-y-scroll">
-                
-
-                {maxActivityData && maxActivityData.map((maxActivity:object, idx:string) => (
-                
-                        <div key={idx} className="flex flex-row items-center w-full max-sm:px-4 rounded-md py-2">
-                            <Avatar>
-                                <AvatarImage src="https://avatars.githubusercontent.com/u/6318762?v=4&size=64" alt="@shadcn" />
-                                <AvatarFallback>CN</AvatarFallback>
-                            </Avatar>
-                            <div className="flex justify-between w-full items-center">
-                                <p className="pl-4 text-sm"><a href="mailto:{maxActivity.userEmail}" className="text-blue-500">{maxActivity.userEmail}</a> edited <strong>{maxActivity.name}</strong></p>
-                                <span className="pl-4 text-gray-500 text-sm">{getRandomTime()}</span>
-                            </div>
-                        </div>
-                    ))}
+                        ))}
                     </div>
-            
-            </div>
-                <div  className="flex flex-col space-y-3 w-full max-sm:px-4">
+                </div>
+
+                <div className="flex flex-col space-y-3 w-full max-sm:px-4">
+                    <h2 className='overViewTitle'>{t('relationsGraph')}</h2>
+                    <div className="flex items-center justify-center  animate-pulse h-auto w-full rounded-xl">
+                        <DataChart />
+                    </div>
+                </div>
+
+                <div className="flex flex-col space-y-3 w-full max-sm:px-4 overflow-hidden">
+                    <h2 className='overViewTitle'>{t('happening')}</h2>
+                    <div className="flex flex-col items-start justify-start h-[300px] w-full rounded-xl gap-1 overflow-y-scroll">
+
+
+                        {maxActivityData && maxActivityData.map((maxActivity: object, idx: string) => (
+
+                            <div key={idx} className="flex flex-row items-center w-full max-sm:px-4 rounded-md py-2">
+                                <Avatar>
+                                    <AvatarImage src="https://avatars.githubusercontent.com/u/6318762?v=4&size=64" alt="@shadcn" />
+                                    <AvatarFallback>CN</AvatarFallback>
+                                </Avatar>
+                                <div className="flex justify-between w-full items-center">
+                                    <p className="pl-4 text-sm"><a href="mailto:{maxActivity.userEmail}" className="text-blue-500">{maxActivity.userEmail}</a> edited <strong>{maxActivity.name}</strong></p>
+                                    <span className="pl-4 text-gray-500 text-sm">{getRandomTime()}</span>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+
+                </div>
+                <div className="flex flex-col space-y-3 w-full max-sm:px-4">
                     <h2 className='overViewTitle'>{t('pageType')}</h2>
                     <div className="flex items-center justify-center animate-pulse bg-muted h-[300px] w-full rounded-xl">
-                    <h3 className="text-white">Panel</h3> 
+                        <h3 className="text-white">Panel</h3>
                     </div>
                 </div>
             </div>
