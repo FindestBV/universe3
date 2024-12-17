e257575896640f5ff714d25fb263563ecc2e4117
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2024-12-17 19:37:58 +0100

    adjust manual height on the display boxes - dashboard svg. Close enough

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -176,7 +176,7 @@ export const Dashboard = () => {
 
         <div className="flex w-full flex-col space-y-3 max-sm:px-4">
           <h2 className="overViewTitle">{t("pageType")}</h2>
-          <div className="flex h-[500px] w-full items-center justify-center rounded-xl">
+          <div className="flex h-[350px] w-full items-center justify-center rounded-xl">
             <div className="pageTypeGraph">
               {typesDataLoading && (
                 <div className="absolute inset-0 z-10 flex flex-col items-center justify-center bg-white bg-opacity-50">
