02ae7aa9c0cc48e01b4d535e90ae0a6a05965631
Ronan O'Leary <roleary81@gmail.com>
2024-12-17 23:52:30 +0100

    change main stylesheet to universe.scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
+import "@/styles/universe.scss";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import AppWrapper from "./App";
-import "@/styles/globals.scss";
 import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppWrapper />
-  </StrictMode>
+  </StrictMode>,
 );
