a329cc2a2d16e2daf5986ec5cd253cb3d9c94bdc
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2024-12-17 01:17:32 +0100

    cleanup deprecated stubs, searchbar and wires up recent activity dropdown

--- src/views/Queries.tsx
+++ src/views/Queries.tsx
@@ -1,33 +1,30 @@
-
-import { StudiesIndex } from "@/components/studies-index";
 import { ListPagination } from "@/components/list-pagination";
 
-
 export const Queries = () => (
-    <div className="flex flex-col w-full h-full max-sm:px-4 px-12">
-        <div className="flex w-full justify-between items-center">
-            <h1 className="text-black font-black text-xl">Queries</h1>
-            <div className="flex">
-            <ListPagination 
-                currentPage={1}
-                totalPages={1} 
-                onPageChange={() => {}}
-                onNextPage={() => {}}
-                onPreviousPage={() => {}}
-            />   
-            </div>  
-        </div>
-        <StudiesIndex />
-        <div className="py-4 w-full">
-            <ListPagination
-                currentPage={1}
-                totalPages={1}
-                onPageChange={() => {}}
-                onNextPage={() => {}}
-                onPreviousPage={() => {}}
-            />     
-        </div>  
+  <div className="flex h-full w-full flex-col px-12 max-sm:px-4">
+    <div className="flex w-full items-center justify-between">
+      <h1 className="text-xl font-black text-black">Queries</h1>
+      <div className="flex">
+        <ListPagination
+          currentPage={1}
+          totalPages={1}
+          onPageChange={() => {}}
+          onNextPage={() => {}}
+          onPreviousPage={() => {}}
+        />
+      </div>
     </div>
+
+    <div className="w-full py-4">
+      <ListPagination
+        currentPage={1}
+        totalPages={1}
+        onPageChange={() => {}}
+        onNextPage={() => {}}
+        onPreviousPage={() => {}}
+      />
+    </div>
+  </div>
 );
 
 export default Queries;
