e2f7f49d6bd348a018ca8b6621101bad95bec7fe
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2024-12-17 01:36:02 +0100

    update dashboard header with recent activity dropdown and element stub

--- src/components/dashboard-header.tsx
+++ src/components/dashboard-header.tsx
@@ -83,8 +83,28 @@ export const DashboardHeader = () => {
                       key={idx}
                       className="cursor-pointer items-center justify-between rounded-md px-2 py-1 text-black hover:bg-gray-100"
                     >
-                      <div className="flex w-full items-center justify-between">
-                        <p>{activity.name}</p>
+                      <div className="flex w-full items-center justify-between gap-4">
+                        <div className="flex items-center gap-4 px-1">
+                          <svg
+                            aria-hidden="true"
+                            focusable="false"
+                            data-prefix="fas"
+                            data-icon="dice-d6"
+                            className="svg-inline--fa fa-dice-d6 objectItem_objectIcon__xwkQs"
+                            width="12px"
+                            role="img"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 448 512"
+                          >
+                            <path
+                              fill="currentColor"
+                              d="M201 10.3c14.3-7.8 31.6-7.8 46 0L422.3 106c5.1 2.8 8.3 8.2 8.3 14s-3.2 11.2-8.3 14L231.7 238c-4.8 2.6-10.5 2.6-15.3 0L25.7 134c-5.1-2.8-8.3-8.2-8.3-14s3.2-11.2 8.3-14L201 10.3zM23.7 170l176 96c5.1 2.8 8.3 8.2 8.3 14l0 216c0 5.6-3 10.9-7.8 13.8s-10.9 3-15.8 .3L25 423.1C9.6 414.7 0 398.6 0 381L0 184c0-5.6 3-10.9 7.8-13.8s10.9-3 15.8-.3zm400.7 0c5-2.7 11-2.6 15.8 .3s7.8 8.1 7.8 13.8l0 197c0 17.6-9.6 33.7-25 42.1L263.7 510c-5 2.7-11 2.6-15.8-.3s-7.8-8.1-7.8-13.8l0-216c0-5.9 3.2-11.2 8.3-14l176-96z"
+                            ></path>
+                          </svg>
+                          {activity.type}
+
+                          <p>{activity.name}</p>
+                        </div>
                         <Button className="py-1">
                           {activityTypeMapping[activity.activityType] || "Unknown"}
                         </Button>
