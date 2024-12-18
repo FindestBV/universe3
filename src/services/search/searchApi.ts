cca4ea6cc5318b93b84931dbf5f12c1c874f90ed
Ronan O'Leary <roleary81@gmail.com>
2024-12-18 18:55:12 +0100

    move data-table dir to ui and fix error in console on searchApi

--- src/services/search/searchApi.ts
+++ src/services/search/searchApi.ts
@@ -1,12 +1,14 @@
-import { api } from '../api';
-import type { SavedDocumentResponse } from '@/types/types';
+import type { SavedDocumentResponse } from "@/types/types";
+
+import { api } from "../api";
 
 export const searchApi = api.injectEndpoints({
   endpoints: (builder) => ({
     searchItems: builder.mutation<SavedDocumentResponse, string>({
-      query: (query) => `search/searchbar?query=${query}&api-version=2.0`
+      query: (query) => `search/searchbar?query=${query}&api-version=2.0`,
     }),
   }),
+  overrideExisting: process.env.NODE_ENV === "development", // This allows overrides only in development mode.
 });
 
 export const { useSearchItemsMutation, usePrefetch } = searchApi;
