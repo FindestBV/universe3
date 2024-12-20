6e765f6036ae7161a1f6b3246797c172151bf610
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2024-12-20 14:42:04 +0100

    update report modal scaffold

--- src/components/shared/modals/generate-report-modal.tsx
+++ src/components/shared/modals/generate-report-modal.tsx
@@ -6,14 +6,15 @@ const GenerateReport: React.FC = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button aria-label="Explore">
-          <ClipboardPlus width={20} color="black" />
-        </Button>
+        <div>
+          <button className="peer/menu-button flex h-8 w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm font-bold outline-none ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:!size-8 group-data-[collapsible=icon]:!p-2 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0">
+            <ClipboardPlus className="h-4" />
+            <span>Generate Report</span>
+          </button>
+        </div>
       </DialogTrigger>
       <DialogContent className="h-[80vh] max-w-6xl bg-slate-100">
-        <div className="flex h-full w-full items-start">
-          <h1>EXPLORER MODAL CONTENT HERE.</h1>
-        </div>
+        <p>Report generation Modal</p>
       </DialogContent>
     </Dialog>
   );
