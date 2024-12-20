36a010ede2a96d7048273c34326e098304530e9e
Ronan O'Leary <roleary81@gmail.com>
2024-12-20 13:41:50 +0100

    updarte theme and skeletins

--- src/components/shared/loaders/document-skeleton.tsx
+++ src/components/shared/loaders/document-skeleton.tsx
@@ -1,4 +1,3 @@
-import { Toolbar } from "@/components/shared/layout/toolbar";
 import { Skeleton } from "@/components/ui/skeleton";
 
 interface DocumentSkeletonProps {
@@ -7,45 +6,47 @@ interface DocumentSkeletonProps {
 
 export const DocumentSkeleton: React.FC<DocumentSkeletonProps> = () => {
   return (
-    <div className="mx-auto flex h-full w-full flex-col px-12 py-4 max-sm:px-4">
-      {/* <header className="documentCrud">
-        <Toolbar />
-      </header> */}
+    <div className="flex h-screen w-auto flex-col">
+      <div className="mx-auto flex h-full w-3/4 flex-col px-12 py-4 max-sm:px-4">
+        {/* <header className="documentCrud">
+          <Toolbar />
+        </header> */}
 
-      <div className="flex h-full w-full flex-col p-12 max-sm:px-4">
-        {/* Title Skeleton */}
-        <Skeleton className="mb-2 h-8 w-3/4 animate-pulse bg-gray-100" />
+        <div className="flex h-full w-full flex-col p-12 max-sm:px-4">
+          {/* Title Skeleton */}
+          <Skeleton className="mb-2 h-8 w-3/4 animate-pulse bg-gray-100" />
 
-        {/* Document ID Skeleton */}
-        <div className="mb-2 flex items-center space-x-2">
-          <Skeleton className="h-4 w-24 animate-pulse bg-gray-100" />
-          <Skeleton className="h-4 w-1/4 animate-pulse bg-gray-100" />
-        </div>
+          {/* Document ID Skeleton */}
+          <div className="mb-2 flex items-center space-x-2">
+            <Skeleton className="h-4 w-24 animate-pulse bg-gray-100" />
+            <Skeleton className="h-4 w-1/4 animate-pulse bg-gray-100" />
+          </div>
 
-        {/* Type Skeleton */}
-        <div className="mb-2 flex items-center space-x-2">
-          <Skeleton className="h-4 w-24 animate-pulse bg-gray-100" />
-          <Skeleton className="h-4 w-1/4 animate-pulse bg-gray-100" />
-        </div>
+          {/* Type Skeleton */}
+          <div className="mb-2 flex items-center space-x-2">
+            <Skeleton className="h-4 w-24 animate-pulse bg-gray-100" />
+            <Skeleton className="h-4 w-1/4 animate-pulse bg-gray-100" />
+          </div>
 
-        {/* Abstract Skeleton */}
-        <div className="mb-4 space-y-2">
-          <Skeleton className="h-4 w-full animate-pulse bg-gray-100" />
-          <Skeleton className="h-4 w-5/6 animate-pulse bg-gray-100" />
-          <Skeleton className="h-4 w-4/5 animate-pulse bg-gray-100" />
-          <Skeleton className="h-4 w-4/5 animate-pulse bg-gray-100" />
-          <Skeleton className="h-4 w-4/5 animate-pulse bg-gray-100" />
-          <Skeleton className="h-4 w-4/5 animate-pulse bg-gray-100" />
-        </div>
+          {/* Abstract Skeleton */}
+          <div className="mb-4 space-y-2">
+            <Skeleton className="h-4 w-full animate-pulse bg-gray-100" />
+            <Skeleton className="h-4 w-5/6 animate-pulse bg-gray-100" />
+            <Skeleton className="h-4 w-4/5 animate-pulse bg-gray-100" />
+            <Skeleton className="h-4 w-4/5 animate-pulse bg-gray-100" />
+            <Skeleton className="h-4 w-4/5 animate-pulse bg-gray-100" />
+            <Skeleton className="h-4 w-4/5 animate-pulse bg-gray-100" />
+          </div>
 
-        {/* Connected Objects Header */}
-        <Skeleton className="mb-2 h-6 w-1/3 animate-pulse bg-gray-100" />
+          {/* Connected Objects Header */}
+          <Skeleton className="mb-2 h-6 w-1/3 animate-pulse bg-gray-100" />
 
-        {/* Connected Objects Skeletons */}
-        <div className="space-y-2">
-          {[...Array(3)].map((_, index) => (
-            <Skeleton key={index} className="h-4 w-1/2 animate-pulse bg-gray-100" />
-          ))}
+          {/* Connected Objects Skeletons */}
+          <div className="space-y-2">
+            {[...Array(3)].map((_, index) => (
+              <Skeleton key={index} className="h-4 w-1/2 animate-pulse bg-gray-100" />
+            ))}
+          </div>
         </div>
       </div>
     </div>
