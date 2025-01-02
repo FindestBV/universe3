2a463d2ad00f797b2fc0c6ec7f7689ec2b3dffa2
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2025-01-02 15:50:44 +0100

    add the incumbent CustomImage object parser to the U3 instances

--- src/components/shared/editor/CustomImage.tsx
+++ src/components/shared/editor/CustomImage.tsx
@@ -1,22 +1,104 @@
-import { mergeAttributes, Node } from "@tiptap/core";
+// https://tiptap.dev/docs/examples/experiments/figure
+// node_modules
+import { mergeAttributes, Node, nodeInputRule } from "@tiptap/core";
+import { Content, JSONContent } from "@tiptap/react";
 
-const CustomImage = Node.create({
+declare module "@tiptap/core" {
+  interface Commands<ReturnType> {
+    customImage: {
+      setCustomImage: (options: { src: string; id: string }, content: Content) => ReturnType;
+    };
+  }
+}
+
+export const inputRegex = /!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\)/;
+
+export const CustomImage = Node.create({
   name: "customImage",
-  group: "block",
-  inline: false,
-  draggable: true,
-  addAttributes() {
+
+  addOptions() {
     return {
-      src: { default: null },
-      id: { default: null },
+      HTMLAttributes: {},
     };
   },
-  parseHTML() {
-    return [{ tag: "img[src]" }];
+
+  group: "block",
+
+  content: "inline*",
+
+  draggable: true,
+
+  isolating: true,
+
+  addAttributes() {
+    return {
+      src: {
+        default: null,
+        parseHTML: (element) => element.querySelector("img")?.getAttribute("src"),
+      },
+      id: {
+        default: "",
+      },
+    };
   },
+
+  addCommands() {
+    return {
+      setCustomImage:
+        (options, content) =>
+        ({ commands }) => {
+          return commands.insertContent({
+            type: this.name,
+            attrs: options,
+            content: content as JSONContent[],
+          });
+        },
+    };
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: "figure",
+        contentElement: "figcaption",
+      },
+    ];
+  },
+
   renderHTML({ HTMLAttributes }) {
-    return ["img", mergeAttributes(HTMLAttributes)];
+    return [
+      "figure",
+      { class: "custom-image" },
+      [
+        "img",
+        mergeAttributes(HTMLAttributes, {
+          draggable: false,
+          contenteditable: false,
+          class: "custom-image-img",
+        }),
+      ],
+      [
+        "figcaption",
+        {
+          draggable: false,
+          class: "custom-image-caption",
+        },
+        0,
+      ],
+    ];
+  },
+
+  addInputRules() {
+    return [
+      nodeInputRule({
+        find: inputRegex,
+        type: this.type,
+        getAttributes: (match) => {
+          const [src] = match;
+
+          return { src };
+        },
+      }),
+    ];
   },
 });
-
-export default CustomImage;
