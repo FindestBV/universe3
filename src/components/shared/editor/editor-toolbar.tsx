67b6cf50897e12d04d1d6edf76c2713e15ef09aa
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2025-01-03 13:00:21 +0100

    update generic editor component toolbar to introduce incumbent universe functionality

--- src/components/shared/editor/editor-toolbar.tsx
+++ src/components/shared/editor/editor-toolbar.tsx
@@ -1,4 +1,4 @@
-import { Bot } from "lucide-react";
+import AskIgorModal from "../modals/ask-igor";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const EditorToolbar = ({ editor }: { editor: any }) => {
@@ -38,28 +38,41 @@ export const EditorToolbar = ({ editor }: { editor: any }) => {
     <div className="toolbar w-full border-b border-gray-300 bg-gray-100 px-4 py-2">
       <div className="flex items-center justify-between">
         {/* Left Section: Ask Igor Button */}
-        <div className="flex items-center space-x-2">
-          <button
-            className="flex gap-2 rounded-sm bg-[#3B82f6] px-4 py-2 text-white hover:bg-slate-300 focus:outline-none focus:ring-2 focus:ring-slate-400"
-            aria-label="Ask Igor"
-          >
-            <Bot size={18} />
-            <p>
-              Ask IGOR <sup>AI</sup>
-            </p>
-          </button>
-          <button
-            className="rounded-sm bg-gray-200 bg-white px-4 py-2 text-slate-500 outline hover:bg-slate-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-slate-400"
-            aria-label="Ask Igor"
-          >
-            Search
-          </button>
+        <div className="flex items-center justify-center gap-2">
+          <AskIgorModal />
+          <div className="flex items-center space-x-2">
+            {/* Bold Button */}
+            {buttons.map((btn, index) => (
+              <button
+                key={index}
+                onClick={btn.command}
+                className={`rounded border border-gray-300 bg-white p-2 text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-400 ${btn.isActive && "active"}`}
+                aria-label="Align Left"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 10h18M3 6h18M3 14h10M3 18h10"
+                  />
+                </svg>
+                {btn.label}
+              </button>
+            ))}
+          </div>
         </div>
 
         {/* Right Section: Formatting and Alignment Options */}
         <div className="flex items-center space-x-2">
           {/* Bold Button */}
-          {buttons.map((btn, index) => (
+          {/* {buttons.map((btn, index) => (
             <button
               key={index}
               onClick={btn.command}
@@ -81,7 +94,7 @@ export const EditorToolbar = ({ editor }: { editor: any }) => {
                 />
               </svg>
             </button>
-          ))}
+          ))} */}
           {/* <button
           className="rounded border border-gray-300 bg-white p-2 text-gray-700 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-400"
           aria-label="Bold"
