64dc14105e01f021bc27233a5fa42beaa5d6d055
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2025-01-13 20:38:36 +0100

    further addition and set up of components directories in Storybook style guide

---
+++ src/stories/04-utilities/cards/inbox-card/InboxCard.stories.tsx
@@ -0,0 +1,45 @@
+import GenericCard from "@/components/shared/cards/generic-card";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta = {
+  title: "Components/shared/cards/InboxCard",
+  component: GenericCard,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "A flexible card component for various use cases, built on top of the Shadcn Card component.",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof GenericCard>;
+
+export const Inbox: Story = {
+  args: {
+    id: "1",
+    title: "Example Document",
+    name: "Sample Name",
+    type: "document",
+    itemType: "document",
+    description:
+      '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"This is a sample description for the GenericCard component."}]}]}',
+    dateAdded: new Date().toISOString(),
+    dateCreated: new Date().toISOString(),
+    createdByUsername: "John Doe",
+    url: "https://example.com",
+    abstract: "An abstract summarizing the document content.",
+    isSelected: false,
+    onSelect: (id, selected) => console.log(`Selected: ${selected}, ID: ${id}`),
+    connectedObjects: [],
+    searchInformation: {},
+    linkedCounts: {
+      entityCount: 5,
+      documentCount: 10,
+    },
+  },
+  render: (args) => <GenericCard {...args} />,
+};
