6ae36f49456952432f38aaf5c329dbe4009c8383
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2025-01-13 17:22:55 +0100

    change default order

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,8 +4,33 @@ import { themes } from "@storybook/theming";
 
 const preview: Preview = {
   parameters: {
+    viewport: {
+      disable: true,
+    },
+    chromatic: {
+      disableSnapshot: true,
+      prefersReducedMotion: "reduce",
+    },
     docs: {
       theme: themes.light,
+    },
+    options: {
+      storySort: {
+        order: [
+          "Getting Started",
+          ["Welcome", "Contributing", "Changelog", "Migration Guides", ["V1 to V2"]],
+          "Foundations",
+          ["Accessibility", "Color", "Elevation", "Grid", ["Overview", "*"], "Typography", "Icons"],
+          "Primitives",
+          ["Overview", "*"],
+          "Structure",
+          "Components",
+          ["Overview", "ui", "shared"],
+          "Utilities",
+          "Testing",
+          ["Technical Components", "Components"],
+        ],
+      },
     },
     controls: {
       matchers: {
