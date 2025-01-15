a0122f18a48eaf203303393580626fcad9d7b452
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2025-01-15 13:54:43 +0100

    rough login refactor

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -1,10 +1,9 @@
 import logoUniverse from "@/assets/universe_logo_color.png";
 import Login from "@/components/login";
-import { LoginImageSlider } from "@/components/login/login-image-slider";
 
 export const LoginPage = () => {
   return (
-    <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-screen flex-1 flex-col justify-center bg-gray-100 px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <a href="https://findest.com">
           <img alt="Universe, by Findest" src={logoUniverse} className="mx-auto h-20 w-auto" />
