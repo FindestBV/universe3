ae46625400d525af253837153df467cb347c4fbe
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2025-03-04 16:46:16 +0100

    fix inconsistencies within version

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { defineConfig, loadEnv } from "vite";
 
 // @ts-nocheck abc
 export default ({ mode }) => {
   // Load environment variables
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  const env = loadEnv(mode, process.cwd(), "VITE_");
 
   return defineConfig({
     plugins: [react()],
@@ -15,8 +15,13 @@ export default ({ mode }) => {
       },
     },
     define: {
-      'process.env': env, // Inject environment variables
+      "process.env": env, // Inject environment variables
       global: {},
+    },
+    build: {
+      rollupOptions: {
+        external: ["y-prosemirror"],
+      },
     },
   });
 };
